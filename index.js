6d443e9fa70cd058e6fa6f0fc74bdbdeeb7dc0e3
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-07-07 13:01:57 -0700

    set up if statement for shape designations and template literal for logo file text

--- index.js
+++ index.js
@@ -38,6 +38,27 @@ const questions = [
 
 function writeToFile(fileName, answers){
     //code for what to write to the svg logo file
+    let shapeSVG;
+    if (answers.shapeType === "Triangle"){
+        triangle = new Triangle();
+        shapeColor = triangle.setColor(answers.shapeColor);
+        shapeSVG = triangle.render();
+    } else if (answers.shapeType === "Circle"){
+        circle = new Circle();
+        shapeColor = circle.setColor(answers.shapeColor);
+        shapeSVG = circle.render();
+    } else {
+        square = new Square();
+        shapeColor = square.setColot(answers.shapeColor);
+        shapeSVG = square.render();
+    }
+    let logoText = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    ${shapeSVG}
+  
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.logoTextext}</text>
+  
+    </svg>`
     fs.writeFile(fileName, logoText, err =>{
         err ? console.error(err) : console.log("Generated logo.svg");
     })
