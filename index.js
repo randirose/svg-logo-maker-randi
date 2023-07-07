23e76faaf311c0203814e1b8db9b831c1fd1b90e
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-07-07 14:13:43 -0700

    restructured files, put questions and render svg function in additional files in lib folder, imported/exported appropriate libraries

--- index.js
+++ index.js
@@ -2,63 +2,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const {Triangle,Circle,Square} = require('./lib/shapes.js');
+const questions = require('./lib/questions.js');
+const renderSvg = require('./lib/svg-text.js');
 
-// questions array for inquirer to prompt user with
-const questions = [
-    // logo text
-    {
-        type: "input",
-        name: "logoText",
-        message: "Please enter the text you would like your logo to display (up to 3 characters)"
-    },
-    // text color
-    {
-        type: "input",
-        name: "textColor",
-        message: "Please enter a color for your text (keyword OR hexadecimal number)"
-    },
-    // shape type
-    {
-        type: "list",
-        name: "shapeType",
-        message: "Please choose one of the shapes below for your logo",
-        choices: [
-            "Circle",
-            "Triangle",
-            "Square"
-        ]
-    },
-    // shape color
-    {
-        type: "input",
-        name: "shapeColor",
-        message: "Please enter a color for the shape (keyword OR hexadecimal number)"
-    }
-]
-
+// this function uses the renderSVG
 function writeToFile(fileName, answers){
-    //code for what to write to the svg logo file
-    let shapeSVG;
-    if (answers.shapeType === "Triangle"){
-        const triangle = new Triangle();
-        shapeColor = triangle.setColor(answers.shapeColor);
-        shapeSVG = triangle.render();
-    } else if (answers.shapeType === "Circle"){
-        const circle = new Circle();
-        shapeColor = circle.setColor(answers.shapeColor);
-        shapeSVG = circle.render();
-    } else {
-        const square = new Square();
-        shapeColor = square.setColor(answers.shapeColor);
-        shapeSVG = square.render();
-    }
-    let logoSvgText = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-    ${shapeSVG}
-  
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.logoText}</text>
-  
-    </svg>`
+    let logoSvgText = renderSvg(answers);
     fs.writeFile(fileName, logoSvgText, err =>{
         err ? console.error(err) : console.log("Generated logo.svg");
     })
