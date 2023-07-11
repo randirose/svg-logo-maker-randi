9c2d94a371ddc026bee03a1f995b69b96ff56b3c
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-07-11 13:49:23 -0700

    added and edited comments for clarity and organization

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const {Triangle,Circle,Square} = require('./lib/shapes.js');
 const questions = require('./lib/questions.js');
 const renderSvg = require('./lib/svg-text.js');
 
-// this function uses the renderSVG
+// this function uses the renderSvg() function imported to generate the text for the logo.svg file, which is then used as the content parameter in the fs.writeFile method
+// it uses fs to write a new file called 'logo.svg' (defined when writeToFile() is called in init())
+// error syntax says to console log "Generated logo.svg" if no errors occur
 function writeToFile(fileName, answers){
     let logoSvgText = renderSvg(answers);
     fs.writeFile(fileName, logoSvgText, err =>{
@@ -13,14 +15,15 @@ function writeToFile(fileName, answers){
     })
 }
 
-// this function initializes app and begins asking user questions; calls writeToFile function
+// this function initializes app and begins asking user questions; calls writeToFile() function
 function init() {
     inquirer.prompt(questions)
     .then((answers)=>{
+        // error handling for if user submits more than 3 chars for logo text, console logs msg and restarts init()
         if (answers.logoText.length > 3){
-            // error handling for if user submits too many chars for logo text, logs msg and restarts question prompts
             console.log("Logo text must be 3 characters or less in length, please start over:");
             init();
+        // if user (or once user does, if they needed to restart) submits 3 or fewer characters for logo text, will console log the generated answers object, and call writeToFile()
         } else {
         console.log(answers);
         writeToFile("logo.svg", answers);
